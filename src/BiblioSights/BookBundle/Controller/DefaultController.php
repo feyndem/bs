019af26154579adc4c7a29bf61baae5b0ed90b52
Marcos Pérez <infommpc@gmail.com>
2013-05-15 11:42:12 +0200

    JSON to OpenLayers data
    Send JSON data from controller and adding features to the map

--- src/BiblioSights/BookBundle/Controller/DefaultController.php
+++ src/BiblioSights/BookBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace BiblioSights\BookBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -24,11 +25,23 @@ class DefaultController extends Controller
     public function generateGeoJSONAction () 
     {
         $em = $this->getDoctrine()->getManager();
-        $BookQuery = $em->createQuery('SELECT b FROM BookBundle:Book b WHERE b.id = :id')->setParameter('id', 10);
+        $BookQuery = $em->createQuery('SELECT b FROM BookBundle:Book b WHERE b.id = :id')->setParameter('id', 12);
         $Book = $BookQuery->getSingleResult();
-        $container = $this->container;
-        $serializer = $container->get('jms_serializer');
-        $serialbook= $serializer->serialize($Book, "json");
-        return new JsonResponse (array('book'=>$serialbook));
+        $Book_Markers = $Book->getMarkers();
+        $markers_array = array();
+        foreach ($Book_Markers as $marker) {
+            $point = $marker->getPoint();
+            $marker_element["type"] = "Feature";
+            $geometry = array();
+            $geometry["type"] = "Point";
+            $geometry["coordinates"] = array($point->getLat(), $point->getLng());
+            $geometry["properties"]=array();            
+            $marker_element["geometry"] = $geometry;
+            $markers_array[] = $marker_element;
+        };
+        $result = array("type" => "FeatureCollection", "features" => $markers_array, "crs"=>array("type"=>"name", "properties"=>array("name"=>"urn:ogc:def:crs:OGC:1.3:CRS84")));
+        $result_json = json_encode($result);
+        $response = new Response($result_json,200,array('content-type' => 'application/json'));
+        return $response;
     }
 }
--- src/BiblioSights/OpenLayersBundle/Resources/public/js/BS_OpenLayers.js
+++ src/BiblioSights/OpenLayersBundle/Resources/public/js/BS_OpenLayers.js
@@ -1,15 +1,28 @@
 var map;
 var OSMlayer;
+var markersLayer;
+var addmarkerLayer;
 
 function OpenLayers_init () {
     // Path to public img folder
     OpenLayers.ImgPath = "bundles/openlayers/images/OLimg/";
     // Creando el mapa en el DIV "map"
-    map = new OpenLayers.Map("map");
+    map = new OpenLayers.Map("map", {
+        projection: new OpenLayers.Projection('ESPG:900913'),
+        maxExtent: new OpenLayers.Bounds(-20037508, -20037508, 20037508, 20037508.34)
+    });
     // Creando la capa base OSM
     OSMlayer = new OpenLayers.Layer.OSM("Base Layer");
+    // Creando la capa vectorial que carga los datos vía geojson
+    markersLayer = new OpenLayers.Layer.Vector("Markers", {
+        strategies: [new OpenLayers.Strategy.Fixed()],
+        protocol: new OpenLayers.Protocol.HTTP({
+            url: "../book/json/",
+            format: new OpenLayers.Format.GeoJSON()
+        })
+    });
     // Creando la capa vectorial que permita añadir un punto
-    var addmarkerLayer = new OpenLayers.Layer.Vector("Added");
+    addmarkerLayer = new OpenLayers.Layer.Vector("Added");
     // Añadiendo capacidad para dibujar puntos
     var drawPoint = new OpenLayers.Control.DrawFeature(addmarkerLayer, OpenLayers.Handler.Point, {
         featureAdded: getInfo
@@ -18,6 +31,7 @@ function OpenLayers_init () {
     drawPoint.activate();
     // Añadiendo capas
     map.addLayer(OSMlayer);
+    map.addLayer(markersLayer);
     map.addLayer(addmarkerLayer);
     map.zoomToMaxExtent();
     // getInfo: 
@@ -45,7 +59,7 @@ function OpenLayers_init () {
                });               
            }
         });
-    }    
+    };
 }
 
 
