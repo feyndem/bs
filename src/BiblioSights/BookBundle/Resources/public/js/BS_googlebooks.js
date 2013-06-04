744129aef2e5e12eb33e5498936f6b7a2a5bd931
Marcos Pérez <infommpc@gmail.com>
2013-06-04 20:38:42 +0200

    Add fields for authors
    Add fields for authors taken from google books. TODO: Remove fields
    functionality

--- src/BiblioSights/BookBundle/Resources/public/js/BS_googlebooks.js
+++ src/BiblioSights/BookBundle/Resources/public/js/BS_googlebooks.js
@@ -1,3 +1,19 @@
+/*
+ * Adición del enlace para añadir varios autores
+ * @var collectionHolfer: selecciona el campo que muestra el campo del fomulario
+ * @var addAuthorLink: HTML del enlace
+ * @var newLinkli 
+ */
+var collectionHolder = $('ul.authors');
+var $addAuthorLink = $('<a href="#" class="add_author_link">Añade más autores</a>');
+var $newLinkLi = $('<p></p>').append($addAuthorLink);
+collectionHolder.append($newLinkLi);
+collectionHolder.data('index', collectionHolder.find(':input').length);
+$addAuthorLink.on('click', function (e) {
+    e.preventDefault();
+    addTagForm(collectionHolder, $newLinkLi);
+});
+
 $('#googlebook').on('click', function (event) {
     event.preventDefault();
     var title = $('#Book_ISBNs_0_Title').val();
@@ -11,56 +27,65 @@ $('#googlebook').on('click', function (event) {
                 var volume = value;
                 
                 $.each(volume, function (index, value) {
-                    volumeObj(index, value);
+                    volumeObj(index, value, GoogleDiv);
                 });                             
             });
         }
+    }); 
+});
+
+function volumeObj(index, value, GoogleDiv) {
+    var newdiv = document.createElement('div');
+    newdiv.id = "googlebooks_"+index;
+    newdiv.setAttribute("data-bstitle",value["volumeInfo"]["title"]);
+    newdiv.setAttribute("data-bspublishedDate",value["volumeInfo"]["publishedDate"]);
+
+
+    var authors = value["volumeInfo"]["authors"];
+    var authorsString = "";
+    $.each(authors, function (index, value) {
+        authorsString += value+"__";
     });
-    
-    function volumeObj(index, value) {
-        var newdiv = document.createElement('div');
-        newdiv.id = "googlebooks_"+index;
-        newdiv.setAttribute("data-bstitle",value["volumeInfo"]["title"]);
-        newdiv.setAttribute("data-bspublishedDate",value["volumeInfo"]["publishedDate"]);
+    newdiv.setAttribute("data-bsauthor", authorsString); 
 
+    var ISBNs = value["volumeInfo"]["industryIdentifiers"];
+    $.each(ISBNs, function (index, value) {
+        newdiv.setAttribute("data-bs"+value["type"], value["identifier"]);
+    });                   
 
-        var authors = value["volumeInfo"]["authors"];
-        $.each(authors, function (index, value) {
-            newdiv.setAttribute("data-bsauthor"+index, value); 
-        });
+    newdiv.innerHTML = JSON.stringify(value["volumeInfo"]["title"]);
+    GoogleDiv.append(newdiv);
+    $("#googlebooks_"+index).on('click', function (event) {
+       var titleField = $('#Book_ISBNs_0_Title');
+       var isbn13Field = $('#Book_ISBNs_0_ISBN13');
+       var isbn10Field = $('#Book_ISBNs_0_ISBN10');
+       var editionyearField = $('#Book_ISBNs_0_EditionYear');
+       
+       // Process string to get an array of authors
+       var bsauthors = $(this).attr('data-bsauthor');
+       bsauthors = bsauthors.substring(0, bsauthors.length-2);
+       var authorsArray = bsauthors.split("__");
+       alert(authorsArray);
+       
+       $.each(authorsArray, function (index, value) {
+           if (index === 0) {
+               $('#Book_authors_'+index+"_name").val(value);
+           } else {
+               addTagForm(collectionHolder, $newLinkLi);
+               $('#Book_authors_'+index+"_name").val(value);
+           }
+       });       
 
-        var ISBNs = value["volumeInfo"]["industryIdentifiers"];
-        $.each(ISBNs, function (index, value) {
-            newdiv.setAttribute("data-bs"+value["type"], value["identifier"]);
-        });                   
+       titleField.val($(this).attr('data-bstitle'));           
+       isbn13Field.val($(this).attr('data-bsisbn_13'));           
+       isbn10Field.val($(this).attr('data-bsisbn_10'));           
+       editionyearField.val($(this).attr('data-bspublisheddate').substring(0,4));           
+    });
+};  
 
-        newdiv.innerHTML = JSON.stringify(value["volumeInfo"]["title"]);
-        GoogleDiv.append(newdiv);
-        $("#googlebooks_"+index).on('click', function (event) {
-           alert("clicked"+index); 
-           var titleField = $('#Book_ISBNs_0_Title');
-           var isbn13Field = $('#Book_ISBNs_0_ISBN13');
-           var isbn10Field = $('#Book_ISBNs_0_ISBN10');
-           var editionyearField = $('#Book_ISBNs_0_EditionYear');
-           
-           titleField.val($(this).attr('data-bstitle'));           
-           isbn13Field.val($(this).attr('data-bsisbn_13'));           
-           isbn10Field.val($(this).attr('data-bsisbn_10'));           
-           editionyearField.val($(this).attr('data-bspublisheddate').substring(0,4));           
-        });
-    };    
-});
-
-var collectionHolder = $('ul.authors');
-var $addAuthorLink = $('<a href="#" class="add_author_link">Añade más autores</a>');
-var $newLinkLi = $('<p></p>').append($addAuthorLink);
-
-collectionHolder.append($newLinkLi);
-collectionHolder.data('index', collectionHolder.find(':input').length);
-$addAuthorLink.on('click', function (e) {
-    e.preventDefault();
-    addTagForm(collectionHolder, $newLinkLi);
-});
+/*
+ * @Function addTagForm
+ */
 
 function addTagForm(collectionHolder, $newLinkLi) {
     // Get the data-prototype explained earlier
